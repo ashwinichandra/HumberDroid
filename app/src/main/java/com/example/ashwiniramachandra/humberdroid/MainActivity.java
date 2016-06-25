c7f6bf8293b1068b8438d0c073475ea20fd1c52f
ashwinichandra <ashwiniramachandra@Ashwinis-MacBook-Pro.local>
2016-06-25 00:15:24 -0400

    added comments

--- app/src/main/java/com/example/ashwiniramachandra/humberdroid/MainActivity.java
+++ app/src/main/java/com/example/ashwiniramachandra/humberdroid/MainActivity.java
@@ -1,19 +1,18 @@
 package com.example.ashwiniramachandra.humberdroid;
 
-import android.app.AlertDialog;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Debug;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.RadioButton;
+
+//Ashiwni Ramachandra
 
 public class MainActivity extends AppCompatActivity  implements View.OnClickListener {
 
